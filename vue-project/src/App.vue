6d62b00020b9211bd2148f2ecb789dc44af69e27
Maxim <xonskyfilms@gmail.com>
2024-05-02 16:19:44 +0300

    feat: add maket app

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,38 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Elevator from './components/elevator.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <div class="flex">
+      <Elevator />
+      <div class='floors'>
+        <div v-for="floor in 5">
+          <TheWelcome :floor=floor />
+        </div>
+
+      </div>
+    </div>
+
+
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.floors {
+  width: 90%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.flex {
+
+  display: flex;
+  margin-left: 200px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.floors {
+  position: absolute;
+  bottom: 0;
 }
 </style>
